bd6bcd0a0a088ef1cafca3ad050995eb6a1e9ce9
saichand67211 <105431268+saichand67211@users.noreply.github.com>
2022-07-05 10:59:04 +0530

    Spring_security

---
+++ spring-boot-security/src/main/java/com/zensar/config/MyConfig.java
@@ -0,0 +1,31 @@
+package com.zensar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MyConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("Sai").password(this.passwordEncoder().encode("pqr")).roles("Root");
+		auth.inMemoryAuthentication().withUser("Kadiyala").password(this.passwordEncoder().encode("xyz")).roles("Temp1234");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
+	}
+
+
+} 
